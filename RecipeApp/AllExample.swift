ef1e227cc26e20e2a6a5bbb4ed54ca3e3a054b6c
rosatiara <rosatiaragaluh@gmail.com>
2024-11-04 13:06:46 +0700

    feat: use instructionsbuilder for recipe and add other kinds of enum for examples

---
+++ RecipeApp/AllExample.swift
@@ -0,0 +1,8 @@
+//
+//  AllExample.swift
+//  RecipeApp
+//
+//  Created by Rosa Tiara Galuh on 04/11/24.
+//
+
+import Foundation
--- RecipeApp/Models/Recipe.swift
+++ RecipeApp/Models/Recipe.swift
@@ -17,28 +17,47 @@ struct Recipe: Identifiable, Codable {
     var isFavorite: Bool = false
 
     enum Difficulty: String, Codable {
-        case easy,medium, hard
+        case easy, medium, hard
+    }
+
+    enum KindOfRecipe: String, Codable {
+        case regular, vegan, fatFree, carbFree
+    }
+
+    init(id: UUID = UUID(),
+         name: String,
+         ingredients: [Ingredient],
+         @InstructionsBuilder instructions: () -> [String],
+         cookingTime: Int,
+         difficulty: Difficulty,
+         isFavorite: Bool = false) {
+        self.id = id
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions()
+        self.cookingTime = cookingTime
+        self.difficulty = difficulty
+        self.isFavorite = isFavorite
     }
 
     static func getDefaultRecipes() -> [Recipe] {
         return [
-            .init(id: UUID(),
-                  name: "Fried Rice",
-                  ingredients: [],
-                  instructions: [
-                    #"""
-                        First we crack some egg
-                        Then we add some water
-                        Then we add some rice
-                        Then we add some salt
-                        Then we add some pepper
-                        Stir till it smells great 😁
-                        Plate it
-                        Enjoy your whats so called """FRIED RICE"""!
-                    """#
-                  ],
-                  cookingTime: 40,
-                  difficulty: .easy)
+            Recipe(
+                name: "Fried Rice",
+                ingredients: [],
+                instructions: {
+                    "First we crack some egg"
+                    "Then we add some water"
+                    "Then we add some rice"
+                    "Then we add some salt"
+                    "Then we add some pepper"
+                    "Stir till it smells great 😁"
+                    "Plate it"
+                    "Enjoy your whats so called \"FRIED RICE\"!"
+                },
+                cookingTime: 40,
+                difficulty: .easy
+            )
         ]
     }
 }
