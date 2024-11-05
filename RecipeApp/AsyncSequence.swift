d698adc28c8efe0a8d7a6074eee135cecb037005
rosatiara <rosatiaragaluh@gmail.com>
2024-11-05 12:33:06 +0700

    feat: add asyncsequence examples

--- RecipeApp/AsyncSequence.swift
+++ RecipeApp/AsyncSequence.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 // MARK: - AsyncSequence for Recipe Search
+
 struct RecipeSearchSequence: AsyncSequence {
     typealias Element = [Recipe]
 
@@ -34,4 +35,63 @@ struct RecipeSearchSequence: AsyncSequence {
     func makeAsyncIterator() -> AsyncIterator {
         AsyncIterator(searchTerm: searchTerm, recipeService: recipeService)
     }
+
+    func stream() -> AsyncThrowingStream<Recipe, Error> {
+        AsyncThrowingStream { continuation in
+            Task {
+                do {
+                    let allRecipes = try await recipeService.fetchRecipes()
+                    for recipe in allRecipes where recipe.name.localizedCaseInsensitiveContains(searchTerm) {
+                        continuation.yield(recipe)
+                    }
+                    continuation.finish()
+                } catch {
+                    continuation.finish(throwing: error)
+                }
+            }
+        }
+    }
+
+    func fetchRecipes() async throws -> [Recipe] {
+            /// ==========================================
+            /// this simulates a network fetch with a potential failure.
+            /// ==========================================
+
+          return try await withUnsafeThrowingContinuation { continuation in
+              /// simulating an asynchronous network call that can fail.....
+              DispatchQueue.global().async {
+                  Thread.sleep(forTimeInterval: 1) /// simulate network delay
+
+                  /// simulate success
+                  let recipes = [
+                    Recipe(
+                        name: "Fried Rice",
+                        ingredients: [],
+                        instructions: {
+                            """
+                            1. Crack some egg
+                            2. Add some water
+                            3. Add some rice
+                            4. Then we add some salt
+                            5. Then we add some pepper
+                            6. Stir till it smells great 😁
+                            7. Plate it
+                            8. Enjoy your what's so called "FRIED RICE"!
+                            """
+                        },
+                        cookingTime: 40,
+                        difficulty: .easy,
+                        kind: .regular
+                    )
+                  ]
+                  /// simulate success (call continuation.resume(returning:))
+                  continuation.resume(returning: recipes)
+
+                  /// if there was an error,
+                  ///continuation.resume(throwing: someError)
+              }
+          }
+      }
 }
+
+
